caea9eba1aa625b9712f6a7a49bf13b9a701ecdc
JISUlicious <ljshong@hanmail.net>
2023-10-03 18:07:15 +0900

    CHANGE: get req.userId from token validation

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,23 +1,20 @@
 const { cartsService } = require("../services");
 
 const getCart = async (req, res) => {
-  //   const { userId } = req;
-  const userId = req.get("userId");
+  const { userId } = req;
   const cart = await cartsService.getCart(userId);
   return res.status(200).json({ message: "querySuccess", data: cart });
 };
 
 const addToCart = async (req, res) => {
-  //   const { userId } = req;
-  const userId = req.get("userId");
+  const { userId } = req;
   const { productId } = req.body;
   await cartsService.addToCart(userId, productId);
   return res.status(201).json({ message: "cartItemCreated" });
 };
 
 const getCartItemCount = async (req, res) => {
-  //   const { userId } = req;
-  const userId = req.get("userId");
+  const { userId } = req;
   const cart = await cartsService.getCart(userId);
   return res.status(200).json({ message: "querySuccess", cartItemCount: cart.length });
 };
--- src/controllers/likes.controller.js
+++ src/controllers/likes.controller.js
@@ -3,7 +3,7 @@ const { likesService } = require("../services");
 const createLike = async (req, res) => {
   try {
     const { productId } = req.params;
-    const userId = req.get("userId");
+    const userId = req.userId;
     await likesService.createLike(userId, productId);
     res.status(201).json({ message: "likeCreated" });
   } catch (error) {
@@ -15,7 +15,7 @@ const createLike = async (req, res) => {
 const deleteLike = async (req, res) => {
   try {
     const { productId } = req.params;
-    const userId = req.get("userId");
+    const userId = req.userId;
     await likesService.deleteLike(userId, productId);
     res.status(204).json({ message: "likeDeleted" });
   } catch (error) {
