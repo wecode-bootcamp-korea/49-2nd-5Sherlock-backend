00de3637d3740859ffee5f133148a311c97278c8
SooahKim <22tndk@gamil.com>
2023-10-04 14:55:35 +0900

    CREATE: createDestination

---
+++ src/services/destinations.service.js
@@ -0,0 +1,34 @@
+// const { checkEmptyValues } = require("../utils/checkEmptyValues");
+// const { checkExistingUserById } = require("./usersUtils/users.util");
+// const { checkExistingProductById, checkExistingLike } = require("./productsUtils/products.util");
+
+const { destinationsModel, usersModel } = require("../models");
+const { throwError } = require("../utils/throwError");
+
+const getAddress = async (userId) => {
+    if (!userId) {
+        throwError(400, "KEY_ERROR");
+    }
+
+    const data = await destinationsModel.getAddress(userId);
+    return data;
+}
+
+const createAddress = async (address,addressName,receiverName,receiverPhoneNumber,userId) => {
+    if (!address || !addressName || !receiverName || !receiverPhoneNumber || !userId) {
+        throwError(400, "KEY_ERROR");
+    }
+
+    const user = await usersModel.getUserById(userId);
+    if (!user) {
+        throwError(404, "USER_NOT_FOUND");
+    }
+
+    await destinationsModel.createAddress(address,addressName,receiverName,receiverPhoneNumber,userId);
+}
+
+
+module.exports = {
+    getAddress,
+    createAddress,
+  };
