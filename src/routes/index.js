13d8b68c894269ce563ab69c1745a42c6327d953
JISU <52498558+JISUlicious@users.noreply.github.com>
2023-10-05 11:21:01 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,12 @@ const { destinationsRouter } = require("./destinations.router");
 const { cartsRouter } = require("./carts.router");
 const { usersRouter } = require("./users.router");
 const { ordersRouter } = require("./orders.router");
+const { validateToken } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.use("/products", productsRouter);
-router.use("/orders", ordersRouter);
+router.use("/orders", validateToken, ordersRouter);
 router.use("/users", usersRouter); 
 router.use("/products", productsRouter); 
 router.use("/destinations", destinationsRouter);
