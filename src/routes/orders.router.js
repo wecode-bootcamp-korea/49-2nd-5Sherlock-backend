f9e2262c51ca53f861dc61b3339e946e1fd2d289
JISU <52498558+JISUlicious@users.noreply.github.com>
2023-10-05 11:24:00 +0900

    Update orders.router.js

--- src/routes/orders.router.js
+++ src/routes/orders.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const { ordersController } = require("../controllers");
+const { validateToken } = require("../middleware/auth");
 
 const ordersRouter = express.Router();
-ordersRouter.post("/", ordersController.createOrder);
+ordersRouter.post("/", validateToken, ordersController.createOrder);
 
 module.exports = {
   ordersRouter,
